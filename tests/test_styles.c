63f20955271383ac72948b67dc3b295054e0957a
Corvin Gröning <114770097+cgroening@users.noreply.github.com>
2026-05-21 06:14:14 +0200

    Refactor test_main.c and test_styles.c to follow clean code principles

--- tests/test_styles.c
+++ tests/test_styles.c
@@ -6,16 +6,49 @@ typedef struct { char text[30]; ScOptions options; } Example;
 typedef struct { Example *items; size_t n; } ExampleGroup ;
 
 
+static Example *get_basic_examples(size_t *count);
+static Example *get_combination_examples(size_t *count);
 static void print_example_groups(const ExampleGroup *examples, size_t n);
+static void print_example_row(ExampleGroup example);
 static void print_example(const Example example, const int spaces_after);
 
 
 void test_styles(void) {
+    size_t basic_examples_count;
+    Example *basic_examples = get_basic_examples(&basic_examples_count);
+
+    size_t comb_examples_count;
+    Example *comb_examples = get_combination_examples(&comb_examples_count);
+
+    ExampleGroup examples[] = {
+        { basic_examples, basic_examples_count },
+        { comb_examples, comb_examples_count },
+    };
+
+    print_example_groups(examples, sizeof(examples) / sizeof(examples[0]));
+}
+
+
+static Example *get_basic_examples(size_t *count) {
     ScOptions plain       = { SC_STYLE_NONE,   SC_COLOR_NONE, SC_COLOR_NONE };
     ScOptions dim         = { SC_STYLE_DIM,    SC_COLOR_NONE, SC_COLOR_NONE };
     ScOptions bold        = { SC_STYLE_BOLD,   SC_COLOR_NONE, SC_COLOR_NONE };
     ScOptions italic      = { SC_STYLE_ITALIC, SC_COLOR_NONE, SC_COLOR_NONE };
     ScOptions underline   = { SC_STYLE_UNDER,  SC_COLOR_NONE, SC_COLOR_NONE };
+
+    static Example examples[5];
+    examples[0] = (Example){ "Plain text", plain };
+    examples[1] = (Example){ "Dim text", dim };
+    examples[2] = (Example){ "Bold text", bold };
+    examples[3] = (Example){ "Italic text", italic };
+    examples[4] = (Example){ "Underlined text", underline };
+
+    *count = sizeof(examples) / sizeof(examples[0]);
+    return examples;
+}
+
+
+static Example *get_combination_examples(size_t *count) {
     ScOptions bold_italic = {
         SC_STYLE_BOLD | SC_STYLE_ITALIC, SC_COLOR_NONE, SC_COLOR_NONE
     };
@@ -28,44 +61,32 @@ void test_styles(void) {
         SC_COLOR_NONE
     };
 
-    Example examples_basic[] = {
-        { "Plain text",             plain },
-        { "Dim text",               dim },
-        { "Bold text",              bold },
-        { "Italic text",            italic },
-        { "Underlined text",        underline },
-    };
+    static Example examples[3];
+    examples[0] = (Example){ "Bold + Italic", bold_italic };
+    examples[1] = (Example){ "Bold + Underlined", bold_under };
+    examples[2] = (Example){ "Bold + Italic + Under", bold_italic_under };
 
-    Example examples_comb[] = {
-        { "Bold + Italic",          bold_italic },
-        { "Bold + Underlined",      bold_under },
-        { "Bold + Italic + Under",  bold_italic_under }
-    };
-
-    ExampleGroup examples[] = {
-        { examples_basic, sizeof(examples_basic) / sizeof(examples_basic[0]) },
-        { examples_comb, sizeof(examples_comb) / sizeof(examples_comb[0]) }
-    };
-
-    print_example_groups(examples, sizeof(examples) / sizeof(examples[0]));
-
+    *count = sizeof(examples) / sizeof(examples[0]);
+    return examples;
 }
 
 
 static void print_example_groups(const ExampleGroup *examples, size_t n) {
-    int spaces_after = 0;
     for(size_t i = 0; i < n; i++) {
-        for(size_t j = 0; j < examples[i].n; j++) {
-            if(j+1 < examples[i].n) {
-                spaces_after = 4;
-            } else {
-                spaces_after = 0;
-            }
-            print_example(examples[i].items[j], spaces_after);
-        }
-
-        printf("\n");
+        print_example_row(examples[i]);
     }
+}
+
+
+static void print_example_row(ExampleGroup example) {
+    int spaces_after = 0;
+
+    for(size_t i = 0; i < example.n; i++) {
+        if(i+1 < example.n) { spaces_after = 4; } else { spaces_after = 0; }
+        print_example(example.items[i], spaces_after);
+    }
+
+    printf("\n");
 }
 
 
